b2ce6210ff8c6e8479053c99b2552626d98abca5
Jann Launer <32454596+jnnr@users.noreply.github.com>
2023-08-28 17:36:22 +0200

    Add dummy script for offshore boundaries

--- rules/boundaries.smk
+++ rules/boundaries.smk
@@ -4,3 +4,10 @@ rule prepare_coordinates_from_shapefile:
     output: 
         path_coordinates="build/coordinates/europe-98-zones.csv"
     script: "../scripts/prepare_coordinates.py"
+
+rule prepare_offshore_boundaries:
+    input:
+        eez="data/EEZ_land_union_v3_202003/EEZ_land_union_v3_202003",
+    output:
+        boundaries_offshore="build/shapes/boundaries_offshore.geojson"
+    script: "../scripts/prepare_offshore_boundaries.py"
---
+++ scripts/prepare_offshore_boundaries.py
@@ -0,0 +1,31 @@
+r"""
+Inputs
+------
+- eez
+
+Outputs
+-------
+- offshore boundaries
+
+Description
+-----------
+Prepare boundaries for offshore regions.
+
+Based on the eez.
+Can be further segmented by some segmentation approach.
+"""
+import geopandas as gpd
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from lib.helpers import mock_snakemake
+
+        snakemake = mock_snakemake("prepare_shapes")
+
+    eez = gpd.read_file(Path(snakemake.input.eez) / "EEZ_Land_v3_202030.shp")
+
+    gdf = eez
+
+    gdf.to_file(snakemake.output[0])
