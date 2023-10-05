f4dcaddef965dd6cc8e2f794ac33a66f65feb76f
Jann Launer <32454596+jnnr@users.noreply.github.com>
2023-10-05 10:29:36 +0200

    Tweak table

--- scripts/table_area_potential.py
+++ scripts/table_area_potential.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         METER_PER_KM = 1e6
         area_potential = pd.read_csv(path_area_potential)
 
-        table = area_potential.loc[:, ["geoname", "area_km2", "available_area", "available_share"]]
+        table = area_potential.loc[:, ["area_km2", "available_area", "available_share"]]
 
         table["shortname"] = area_potential[["iso_sov1", "iso_sov2"]].apply(format_tuple, axis=1)
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         table.loc[:, "available_share"] = np.round(table.loc[:, "available_share"], 2)
         table.loc[:, "available_area"] = table.loc[:, "available_area"].apply(lambda x: f"{x/METER_PER_KM:.0f}")
 
-        table = table.set_index(["geoname", "shortname"])
+        table = table.set_index(["shortname", "area_km2"])
 
         return table
     
@@ -30,5 +30,9 @@ if __name__ == "__main__":
     table_shallow = prepare_table(snakemake.input.area_offshore_shallow)
 
     table_joined = table_deep.join(table_shallow, lsuffix="_deep", rsuffix="_shallow")
+
+    table_joined = table_joined.sort_values(by=["area_km2"], ascending=False)
+
+    table_joined.columns = [col.replace("_", " ") for col in table_joined.columns]
 
     table_joined.to_csv(str(snakemake.output))
